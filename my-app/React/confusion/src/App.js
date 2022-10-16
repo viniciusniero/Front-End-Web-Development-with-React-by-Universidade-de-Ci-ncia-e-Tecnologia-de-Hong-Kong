99ef204beab4be67944fb284ca1cc8689af4312e
Vinicius Niero <viniciusniero123@gmail.com>
2022-10-16 09:51:30 -0300

    1 tarefa

--- my-app/React/confusion/src/App.js
+++ my-app/React/confusion/src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponet';
-import './App.css';
+import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
+import './App.css';
+import { Component } from 'react';
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
+
     this.state = {
       dishes: DISHES
     };
@@ -15,12 +16,15 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="App">
         <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <div ClassName="container">
+            <NavbarBrand href="/">
+              teste
+            </NavbarBrand>
           </div>
         </Navbar>
+
         <Menu dishes={this.state.dishes} />
       </div>
     );
