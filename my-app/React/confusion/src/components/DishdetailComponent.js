06847f81c6583d65de8882ee8635eb65234d0b19
Vinicius Niero <viniciusniero123@gmail.com>
2022-11-09 21:32:19 -0300

    Fetch from Server

--- my-app/React/confusion/src/components/DishdetailComponent.js
+++ my-app/React/confusion/src/components/DishdetailComponent.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from "reactstrap";
 import {LocalForm, Control, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len)
@@ -100,10 +102,8 @@ class CommentForm extends Component {
 function RenderDish({dish}) {
         return (
             <div className="col-12 col-md-5 m-1">
-                {/* <FadeTransform in
-                 transformProps={{ exitTransform: 'scale(0.5) translateY(-50%)'}}> */}
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
